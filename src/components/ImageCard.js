8a2907ffed409bac323bd8214e65ee420f8572a4
RNtaate <royhyde14@gmail.com>
2023-03-02 15:00:30 +0300

    Add styling and design to the View in imagecard component.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
 
-export default function ImageCard() {
+const { width: screenWidth, height: screenHeight} = Dimensions.get('screen');
+
+const ImageCard = () => {
   return (
-    <View>
+    <View style={styles.imageContainer} >
       <Text>Image Card</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: screenWidth * (2 / 3),
+    height: screenHeight / 2,
+    backgroundColor: "gray",
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 20
+  }
+})
+
+
+export default ImageCard;
