eac513b341a6edaf1893f8f48e6f107f3add90f7
RNtaate <royhyde14@gmail.com>
2023-03-02 16:11:43 +0300

    Create ImageList file to hold the images array

---
+++ src/services/ImagesList.js
@@ -0,0 +1,20 @@
+const images = [
+  {
+    movie: "avengers",
+    posterPath: require("../.././assets/avengers_original.jpg")
+  },
+  {
+    movie: "ultron",
+    posterPath: require("../.././assets/avengers_ultron.jpg")
+  },
+  {
+    movie: "infinity",
+    posterPath: require("../.././assets/avengers_infinity.jpg")
+  },
+  {
+    movie: "endgame",
+    posterPath: require("../.././assets/avengers_endgame.jpg")
+  },
+]
+
+export default images;
