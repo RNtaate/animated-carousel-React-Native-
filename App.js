0ce688cb9069773c5bebbb1e83fd6182bfa46d5b
RNtaate <royhyde14@gmail.com>
2023-03-02 16:01:07 +0300

    Add Flatlist to App component required for the carousel
    Add paths for the images required for the flat list

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import ImageCard from './src/components/ImageCard';
 
-const imagePath = require('./assets/avengers_ultron.jpg')
+const images = [
+  {
+    movie: "avengers",
+    posterPath: require("./assets/avengers_original.jpg")
+  },
+  {
+    movie: "ultron",
+    posterPath: require("./assets/avengers_ultron.jpg")
+  },
+  {
+    movie: "infinity",
+    posterPath: require("./assets/avengers_infinity.jpg")
+  },
+  {
+    movie: "endgame",
+    posterPath: require("./assets/avengers_endgame.jpg")
+  },
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageCard path={imagePath} />
+      <FlatList 
+        data={images}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.movie}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.imageWrapper} >
+              <ImageCard path={item.posterPath} />
+            </View>
+          )
+        }}
+      />
     </View>
   );
 }
