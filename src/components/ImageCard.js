ee5ed664a7a0a2714f456e6ab9cf9af6ac087934
RNtaate <royhyde14@gmail.com>
2023-03-02 14:45:23 +0300

    Create image card component
    Import and use it in the App component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function ImageCard() {
+  return (
+    <View>
+      <Text>Image Card</Text>
+    </View>
+  )
+}
