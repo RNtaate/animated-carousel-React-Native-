9e981a82fe5d651fe0f25b5f17d8114504a46d1e
RNtaate <royhyde14@gmail.com>
2023-03-14 17:18:15 +0300

    Import and use FirstCarousel container in the FirstCarouselScreen

--- src/screens/FirstCarouselScreen.js
+++ src/screens/FirstCarouselScreen.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import FirstCarousel from '../containers/FirstCarousel';
+
 export default function FirstCarouselScreen() {
   return (
-    <View>
-      <Text>
-        First Carousel Screen
-      </Text>
-    </View>
+    <FirstCarousel />
   )
 }
