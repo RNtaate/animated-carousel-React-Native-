9f431f734b3492ed8d0b80e9b22c3aad1fc9ba1f
RNtaate <royhyde14@gmail.com>
2023-03-14 17:52:42 +0300

    Add styling to the screen options of the bottom tab navigator

--- src/screens/index.js
+++ src/screens/index.js
@@ -8,7 +8,23 @@ const Tab = createBottomTabNavigator();
 
 const CarouselTabs = () => {
   return (
-    <Tab.Navigator screenOptions={ { headerShown: false } } >
+    <Tab.Navigator 
+      screenOptions={
+        { 
+          headerShown: false,
+          tabBarActiveTintColor: "red",
+          tabBarInactiveTintColor: "white",
+          tabBarStyle: {
+            height: 60,
+            backgroundColor: "rgba(255, 255, 255, 0.3)",
+            paddingBottom: 5,
+            position: 'absolute',
+            borderTopWidth: 0,
+            elevation: 0
+          }
+        }
+      }
+    >
       <Tab.Screen name='Carousel 1' component={FirstCarouselScreen} />
       <Tab.Screen name="Carousel 2" component={SecondCarouselScreen} />
     </Tab.Navigator>
