64db68c310ecaf04a927fc9c51bd024630eeab39
RNtaate <royhyde14@gmail.com>
2023-03-15 00:47:11 +0300

    Update the project structure and transfer some files to other locations.

--- src/components/FirstCarouselComponents/FirstCarousel.js
+++ src/components/FirstCarouselComponents/FirstCarousel.js
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
 import { StyleSheet, View, Dimensions, Animated } from 'react-native';
 
-import ImageCard from '../components/ImageCard';
-import images from '../services/ImagesList';
+import ImageCard from './ImageCard';
+import images from '../../services/ImagesList';
 
 const { width: screenWidth, height: screenHeight} = Dimensions.get('screen');
 
--- src/screens/FirstCarouselScreen.js
+++ src/screens/FirstCarouselScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-import FirstCarousel from '../containers/FirstCarousel';
+import FirstCarousel from '../components/FirstCarouselComponents/FirstCarousel';
 
 export default function FirstCarouselScreen() {
   return (
