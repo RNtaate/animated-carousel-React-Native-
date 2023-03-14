793c444c70bae4b10fd391fd1b2f498cb9e5be58
RNtaate <royhyde14@gmail.com>
2023-03-14 17:12:52 +0300

    Create the First, Second Carousel Screens

---
+++ src/screens/SecondCarouselScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function SecondCarouselScreen() {
+  return (
+    <View>
+      <Text>
+        Second Carousel Screen
+      </Text>
+    </View>
+  )
+}
