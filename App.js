8507ccd7caa06baefe58b0bea67b42afca6f82b7
RNtaate <royhyde14@gmail.com>
2023-03-14 17:14:44 +0300

    Remove unwanted code

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-// import FirstCarousel from './src/containers/FirstCarousel';
 import CarouselTabs from './src/screens';
 
 export default function App() {
