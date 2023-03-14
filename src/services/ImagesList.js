901b853a65060fae237522312dcf03152d0545b5
RNtaate <royhyde14@gmail.com>
2023-03-15 01:26:57 +0300

    Update the imageList by adding the name property

--- src/services/ImagesList.js
+++ src/services/ImagesList.js
@@ -1,18 +1,22 @@
 const images = [
   {
     movie: "avengers",
+    name: "Marvel's The Avengers",
     posterPath: require("../.././assets/avengers_original.jpg")
   },
   {
     movie: "ultron",
+    name: "Avengers: Age Of Ultron",
     posterPath: require("../.././assets/avengers_ultron.jpg")
   },
   {
     movie: "infinity",
+    name: "Avengers: Infinity War",
     posterPath: require("../.././assets/avengers_infinity.jpg")
   },
   {
     movie: "endgame",
+    name: "Avengers: Endgame",
     posterPath: require("../.././assets/avengers_endgame.jpg")
   },
 ]
