a297bf6ae99def2906bd5dda0eda901c4811252e
RNtaate <royhyde14@gmail.com>
2023-03-14 18:21:02 +0300

    Create createScreenOptions, createTabBarIcon functions
    Add icons to the tab bar using expo vector icons

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, Entypo } from '@expo/vector-icons'
 
 import FirstCarouselScreen from './FirstCarouselScreen';
 import SecondCarouselScreen from './SecondCarouselScreen';
@@ -7,24 +8,40 @@ import SecondCarouselScreen from './SecondCarouselScreen';
 const Tab = createBottomTabNavigator();
 
 const CarouselTabs = () => {
-  return (
-    <Tab.Navigator 
-      screenOptions={
-        { 
-          headerShown: false,
-          tabBarActiveTintColor: "red",
-          tabBarInactiveTintColor: "white",
-          tabBarStyle: {
-            height: 60,
-            backgroundColor: "rgba(255, 255, 255, 0.3)",
-            paddingBottom: 5,
-            position: 'absolute',
-            borderTopWidth: 0,
-            elevation: 0
-          }
+
+  const createTabBarIcon = ( routeName, color ) => {
+    if (routeName == "Carousel 1"){
+      return (
+        <Ionicons name="list" size={24} color={color} />
+      )
+    }else {
+      return (
+        <Entypo name='list' size={24} color={color} />
+      )
+    }
+  }
+
+  const createScreenOptions = ( {route} ) => {
+    return (
+      { 
+        headerShown: false,
+        tabBarActiveTintColor: "orange",
+        tabBarInactiveTintColor: "white",
+        tabBarIcon: ({color}) => (createTabBarIcon(route.name, color)),
+        tabBarStyle: {
+          height: 60,
+          backgroundColor: "rgba(255, 255, 255, 0.3)",
+          paddingBottom: 5,
+          position: 'absolute',
+          borderTopWidth: 0,
+          elevation: 0
         }
       }
-    >
+    )
+  }
+
+  return (
+    <Tab.Navigator screenOptions={ createScreenOptions }>
       <Tab.Screen name='Carousel 1' component={FirstCarouselScreen} />
       <Tab.Screen name="Carousel 2" component={SecondCarouselScreen} />
     </Tab.Navigator>
