b885217e252ea8ce84b724f1911cba4f62c31c7e
RNtaate <royhyde14@gmail.com>
2023-03-14 17:18:36 +0300

    Add styling to the SecondCarouselScreen component

--- src/screens/SecondCarouselScreen.js
+++ src/screens/SecondCarouselScreen.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function SecondCarouselScreen() {
   return (
-    <View>
+    <View style={styles.container} >
       <Text>
         Second Carousel Screen
       </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
