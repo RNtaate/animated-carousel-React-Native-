11581e83193181f7a568c2942413a8efbe44bdd0
RNtaate <royhyde14@gmail.com>
2023-03-22 01:54:53 +0300

    Add the main carousel to the FourthCarousel component.
    Update the POSTER_LINK in the Helpers.js file

--- src/components/FourthCarouselComponents/FourthCarousel.js
+++ src/components/FourthCarouselComponents/FourthCarousel.js
@@ -1,12 +1,45 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Dimensions, Image } from 'react-native';
+
+import movieList from '../../services/movielist.json'
+import { POSTER_LINK } from '../../services/Helpers';
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("screen");
+const moviesData = movieList.results;
 
 const FourthCarousel = () => {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: 'center' }} >
-      <Text>This is the Fourth Carousel</Text>
+    <View style={styles.wrappingContainer} >
+      <FlatList 
+        data={moviesData}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.imageContainer} >
+              <Image source={{uri: `${POSTER_LINK}${item.poster_path}`}} resizeMode="cover" style={styles.upperImage} />
+            </View>
+          )
+        }}
+        pagingEnabled
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrappingContainer: {
+    flex: 1,
+  },
+  imageContainer: {
+    width: screenWidth,
+    height: screenHeight,
+  },
+  upperImage: {
+    width: "100%",
+    height: "100%"
+  }
+})
 
 export default FourthCarousel;
