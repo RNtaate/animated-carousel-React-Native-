2437a7193c803eec36aa7eb6496b4ac667767f9b
RNtaate <royhyde14@gmail.com>
2023-03-22 02:30:05 +0300

    Add lower carousel to Fourth Carousel component.
    Add styling to the Flatlist and its render item of the lower list

--- src/components/FourthCarouselComponents/FourthCarousel.js
+++ src/components/FourthCarouselComponents/FourthCarousel.js
@@ -6,11 +6,14 @@ import { POSTER_LINK } from '../../services/Helpers';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("screen");
 const moviesData = movieList.results;
+const ITEM_WIDTH = 100
+const ITEM_HEIGHT = 100
 
 const FourthCarousel = () => {
   return (
     <View style={styles.wrappingContainer} >
-      <FlatList 
+      <FlatList
+        style={styles.upperList} 
         data={moviesData}
         keyExtractor={item => item.id.toString()}
         horizontal
@@ -24,6 +27,21 @@ const FourthCarousel = () => {
         }}
         pagingEnabled
       />
+
+      <View style={styles.viewCover} ></View>
+
+      <FlatList style={styles.lowerList}
+        data={moviesData}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        renderItem={({item}) => {
+          return(
+          <View style={styles.lowerImageContainer} >
+              <Image source={{uri: `${POSTER_LINK}${item.poster_path}`}} resizeMode="cover" style={styles.lowerImage} />
+          </View>
+          )
+        }}
+      />
     </View>
   )
 }
@@ -36,9 +54,34 @@ const styles = StyleSheet.create({
     width: screenWidth,
     height: screenHeight,
   },
+  viewCover: {
+    position: "absolute",
+    width: screenWidth,
+    height: screenHeight,
+    top: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    pointerEvents: "none"
+  },
+  lowerList: {
+    position: 'absolute',
+    bottom: ITEM_HEIGHT * 0.7,
+  },
+  lowerImageContainer: {
+    width: ITEM_WIDTH * 1.2,
+    height: ITEM_HEIGHT,
+    justifyContent: 'center',
+    alignItems: "center"
+  },
   upperImage: {
     width: "100%",
-    height: "100%"
+    height: "100%",
+  },
+  lowerImage: {
+    width: ITEM_WIDTH,
+    height: "100%",
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "#bbb"
   }
 })
 
