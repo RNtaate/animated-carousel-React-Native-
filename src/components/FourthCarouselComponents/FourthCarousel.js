9580a23cf7aea3e35d676f27eb6b6ede7ec926ff
RNtaate <royhyde14@gmail.com>
2023-03-22 01:12:01 +0300

    Create fourth carousel component

---
+++ src/components/FourthCarouselComponents/FourthCarousel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const FourthCarousel = () => {
+  return (
+    <View style={{flex: 1, justifyContent: "center", alignItems: 'center' }} >
+      <Text>This is the Fourth Carousel</Text>
+    </View>
+  )
+}
+
+export default FourthCarousel;
