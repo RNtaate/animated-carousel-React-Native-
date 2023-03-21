254106d01822636a210e7d1870c8775f83ec4d7d
RNtaate <royhyde14@gmail.com>
2023-03-21 15:24:06 +0300

    Remove all transferred code to Backdrop component from the Third Carousel.

--- src/components/ThirdCarouselComponents/ThirdCarousel.js
+++ src/components/ThirdCarouselComponents/ThirdCarousel.js
@@ -1,72 +1,22 @@
 import React, { useRef } from 'react'
-import { View, StyleSheet, Dimensions, Animated, Image, Platform } from 'react-native';
-import MaskedView from '@react-native-masked-view/masked-view';
-import { Svg, Rect } from 'react-native-svg';
-import { LinearGradient } from 'expo-linear-gradient';
+import { View, StyleSheet, Dimensions, Animated} from 'react-native';
 
 import SecondImageCard from '../SecondCarouselComponents/SecondImageCard';
 import images from '../../services/ImagesList';
+import Backdrop from './Backdrop';
 
 const {width: screenWidth, height: screenHeight} = Dimensions.get('screen')
 const ITEM_SIZE = screenWidth * 0.60
 const SPACER_ITEM_SIZE = (screenWidth - ITEM_SIZE) / 2
-const BACKDROP_HEIGHT = screenHeight * 0.6
 
 const moviesData = [{movie: 'left_spacer'}, ...images, {movie: 'right_spacer'}]
-
-const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
 const ThirdCarousel = () => {
 
   const scrollX = useRef( new Animated.Value(0)).current
   return (
     <View style={styles.container} >
-      <View style={styles.backdropContainer}>
-
-        {
-          moviesData.map((singleMovie, index) => {
-            if(!singleMovie.posterPath) {
-              return null
-            }
-
-            const inputRange = [
-              (index - 2) * ITEM_SIZE,
-              (index - 1) * ITEM_SIZE,
-              index * ITEM_SIZE
-            ]
-
-            const translateX = scrollX.interpolate({
-              inputRange,
-              outputRange: [-screenWidth, 0, screenWidth]
-            })
-
-            if(singleMovie.posterPath) {
-              return (
-                <MaskedView
-                  style={{position: 'absolute', width: "100%", height: "100%", top: 0, left: 0}}
-                  key={singleMovie.movie}
-                  androidRenderingMode={Platform.OS === 'android' ? 'software' : 'hardware'} //This is necessary to animate the "maskElement" on Android
-                  maskElement={
-                    <AnimatedSvg
-                      width={screenWidth}
-                      height={screenHeight}
-                      viewBox={`0 0 ${screenWidth} ${screenHeight}`}
-                      style={{transform: [{translateX}]}}
-                    >
-                      <Rect x="0" y="0" width={screenWidth} height={screenHeight} fill="red" />
-                    </AnimatedSvg>
-                  }
-                >
-                  <Image source={singleMovie.backdrop} resizeMode="cover" style={{width: "100%", height: "100%"}} />
-                </MaskedView>
-              )
-            }
-          })
-        }
-
-        <LinearGradient colors={["transparent", "white"]} style={{position: 'absolute', width: "100%", height: "100%", bottom: 0}} />
-      </View>
-
+      <Backdrop scrollX={scrollX} data={moviesData} />
 
       <Animated.FlatList
         data={moviesData}
@@ -123,16 +73,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  backdropContainer: {
-    position: "absolute",
-    width: screenWidth,
-    top: 0,
-    height: BACKDROP_HEIGHT,
-  },
-  backdropImage: {
-    width: "100%",
-    height: "100%"
-  }
 })
 
 export default ThirdCarousel;
