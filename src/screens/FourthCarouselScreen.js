4cbcb80fdc0d338d33d0ee31d3dda7cfe929903b
RNtaate <royhyde14@gmail.com>
2023-03-22 12:55:16 +0300

    Create FourthCarouselScreen
    Import and add the FourthCarousel to it.

---
+++ src/screens/FourthCarouselScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import FourthCarousel from '../components/FourthCarouselComponents/FourthCarousel';
+
+const FourthCarouselScreen = () => {
+  return (
+    <FourthCarousel />
+  )
+}
+
+export default FourthCarouselScreen;
