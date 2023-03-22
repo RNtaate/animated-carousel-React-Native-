0c30428b40a140b5970e6d2dcd6859b6471a1c95
RNtaate <royhyde14@gmail.com>
2023-03-22 23:18:38 +0300

    Add the first draft of the flatlsit and styling to its components.

--- src/components/ParallaxCarouselComponents/ParallaxCarousel.js
+++ src/components/ParallaxCarouselComponents/ParallaxCarousel.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, Image, FlatList, StyleSheet } from 'react-native';
+import unsplash from '../../services/unsplash_images.json';
+
+const unsplashImages = unsplash.results;
+const { width: screenWidth, height: screenHeight } = Dimensions.get("screen");
 
 const ParallaxCarousel = () => {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}} >
-      <Text>Parallax Carousel</Text>
+    <View style={styles.wrappingContainer} >
+      <FlatList
+        data={unsplashImages}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => {
+          return(
+            <View style={styles.boundingView} >
+              <View style={styles.imageContainer}>
+                <Image source={{uri: item.image}} resizeMode="cover" style={styles.carouselImage} />
+              </View>
+            </View>
+          )
+        }}
+        pagingEnabled
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrappingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee"
+  },
+  boundingView: {
+    width: screenWidth,
+    height: screenHeight,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imageContainer: {
+    width: screenWidth * 0.7,
+    height: screenHeight * 0.4,
+    borderWidth: 10,
+    borderColor: "white",
+    elevation: 15
+  },
+  carouselImage: {
+    width: "100%",
+    height: "100%"
+  }
+})
 
 export default ParallaxCarousel;
